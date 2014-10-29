0c757682051677dffb4808fca951f23a41755eac
Tung Nghiem <tungn@1spatial.com>
2014-10-29 21:01:15 +0000

    routing

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -1,6 +1,41 @@
-'use strict';
+/*global $:false */
 
-angular.module('civweb', ['ui.router'])
-    .controller('MainCtrl', function ($scope, $state) {        
-        $scope.test = 'Hello world';
-});
+'use strict';
+angular.module('game', [ 'ui.router'])  
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'scripts/app/home.html'
+      })
+      .state('game', {
+        url: '/game',
+        templateUrl: 'scripts/app/game.html',
+        resolve: {
+          
+        },
+        controller: function ($scope) {
+            
+        }
+      })
+      .state('game.buildings', {
+        url: '/buildings',
+        templateUrl: 'scripts/app/buildings.html'
+      })
+      .state('game.wonders', {
+        url: '/wonders',
+        templateUrl: 'scripts/app/wonders.html'
+      })
+      .state('game.techs', {
+        url: '/techs',
+        templateUrl: 'scripts/app/techs.html'
+      })
+      .state('game.units', {
+        url: '/units',
+        templateUrl: 'scripts/app/units.html'
+      })
+      ;
+  })
+  .controller('MainCtrl', function($scope, $state) {
+
+  });
